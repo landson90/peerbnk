45e5913a6b9d34e069837dd237b5e230d268298b
landson90 <randelsilva1990@gmail.com>
2022-03-12 19:01:25 -0300

    criando componente da tabela  e adicionando mais item

--- src/components/table/body/index.tsx
+++ src/components/table/body/index.tsx
@@ -14,6 +14,45 @@ export function TableBody() {
           </a>
         </td>
       </tr>
+      <tr>
+        <td className="px-6 py-4 text-sm font-medium text-gray-900 whitespace-nowrap">
+          The Sliding Mr. Bones (Next Stop, Pottersville)
+        </td>
+        <td className="px-6 py-4 text-sm font-medium text-gray-900 whitespace-nowrap">
+          The Sliding Mr. Bones (Next Stop, Pottersville)
+        </td>
+        <td className="px-6 py-4 text-sm font-medium text-gray-900 whitespace-nowrap">
+          <a href="#" className="text-green-600 hover:text-green-900">
+            Detalhes
+          </a>
+        </td>
+      </tr>
+      <tr>
+        <td className="px-6 py-4 text-sm font-medium text-gray-900 whitespace-nowrap">
+          The Sliding Mr. Bones (Next Stop, Pottersville)
+        </td>
+        <td className="px-6 py-4 text-sm font-medium text-gray-900 whitespace-nowrap">
+          The Sliding Mr. Bones (Next Stop, Pottersville)
+        </td>
+        <td className="px-6 py-4 text-sm font-medium text-gray-900 whitespace-nowrap">
+          <a href="#" className="text-green-600 hover:text-green-900">
+            Detalhes
+          </a>
+        </td>
+      </tr>
+      <tr>
+        <td className="px-6 py-4 text-sm font-medium text-gray-900 whitespace-nowrap">
+          The Sliding Mr. Bones (Next Stop, Pottersville)
+        </td>
+        <td className="px-6 py-4 text-sm font-medium text-gray-900 whitespace-nowrap">
+          The Sliding Mr. Bones (Next Stop, Pottersville)
+        </td>
+        <td className="px-6 py-4 text-sm font-medium text-gray-900 whitespace-nowrap">
+          <a href="#" className="text-green-600 hover:text-green-900">
+            Detalhes
+          </a>
+        </td>
+      </tr>
     </tbody>
   );
 }
